5e6e94fae72c5fbee12e60d3ebf9101dc6b063ab
Yozuru <yozuru@huel.com>
2022-11-25 12:33:38 +0800

    实现了 用户信息的查询、修改功能

---
+++ yozuru-framework/src/main/java/com/yozuru/domain/dto/RegisterDto.java
@@ -0,0 +1,27 @@
+package com.yozuru.domain.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @author Yozuru
+ */
+@Data
+@NoArgsConstructor
+public class RegisterUserDto {
+    @NotNull
+    @Length(min = 6,max = 14,message = "用户名的长度应为6~14字符")
+    private String userName;
+    @NotNull
+    @Length(min = 6,max = 14,message = "密码的长度应为6-14字符")
+    private String password;
+    @NotNull
+    @Email
+    private String email;
+    @Length(min = 4,max = 10,message = "昵称的长度应为4-10字符")
+    private String nickName;
+}
